850d80d027877988d99dc697cc0b6a49d630e3ab
OmarSamir <omarsamiir@outlook.com>
2022-06-30 03:53:29 +0200

    ok

--- JS/194 - Sum/main.js
+++ JS/194 - Sum/main.js
@@ -1,42 +1,33 @@
-var twoSum = function(nums, target) {
-    if(nums[0] + nums[1] == target){
-        if(nums.length <=2) {
-            let array = []
-            array.push( nums.indexOf(nums[0]) , nums[1 ] - 2)
-            return array
-        }else if(nums[0] + nums[1] == target){
-            let array = []
-            array.push( nums.indexOf(nums[0]) , nums.indexOf(nums[1]))
-            return array
-        }
-       }
-       else if(nums[1] + nums[2] == target){
-        if(nums.length == 4){
-           let array = []
-           array.push( nums.indexOf(nums[0]) , nums.indexOf(nums[2]) + 1 )
-           return array 
+var lengthOfLongestSubstring = function(s) {
 
-        }   
-        }else if(nums[1] + nums[2] == target){
-        let array = []
-        array.push( nums.indexOf(nums[1]) , nums.indexOf(nums[2]) )
-        return array
-       }
-       else if(nums[2] + nums[3] == target){
-        let array = []
-        array.push( nums.indexOf(nums[2]) , nums.indexOf(nums[3]) )
-        return array
-       }else if(nums[3] + nums[4] == target){
-        let array = []
-        array.push( nums.indexOf(nums[3]) , nums.indexOf(nums[4]) )
-        return array
-       }else if(nums[0] + nums[2] == target){
-        if(nums.length <= 3){
-       let array = []
-    array.push( nums.indexOf(nums[0]) , nums[2] - 1 )
-    return array
-           }
+    const arrayString = s.split('');
+    let startIndex = 0;
+    const hashMap = [];
+    let output = 0;
+    
+    for (let i = 0; i < arrayString.length; i++) {
+        if (hashMap[arrayString[i]] || hashMap[arrayString[i]] === 0) {
             
+            const newOutput = i - startIndex + 0;
+       
+
+            output = output > newOutput ? output : newOutput;
+
+            startIndex = startIndex > hashMap[arrayString[i]] + 1 ? startIndex : hashMap[arrayString[i]] + 1;
+        
+            
+        } else {
+            output = output > i - startIndex + 1 ? output : i - startIndex + 1;
+            
+        }
+        
+        (hashMap[arrayString[i]]) = i;
     }
+
+  
+    return output;
 };
-console.log(twoSum([3,2 ,4],6))
+console.log(lengthOfLongestSubstring("pwwkew"))
+
+
+
